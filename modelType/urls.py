30cdcd3b886cb3e22f8d9ac16a5df4abcdcf382f
Carol Acharya <carolacharya@Carols-MacBook-Air.local>
2023-12-12 10:46:13 +0545

    List and Gereic View initialized

--- modelType/urls.py
+++ modelType/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .views import StudentListView
+from .views import StudentListView,StudentDetailView
 
+# urls.py
 urlpatterns = [
-    path('students/',views.StudentListView.as_view(),name='students')
+    path('students/', StudentListView.as_view(), name='students'),
+    path('students/<int:pk>/', StudentDetailView.as_view(), name='student_detail'),
 ]
--- modelType/views.py
+++ modelType/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import CommonInfo,Student
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -16,3 +17,11 @@ class StudentListView(ListView):
     model = Student
     template_name = 'index.html'
     context_object_name = 'students'
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'student_detail.html'  # Create this template
+
+    def get_success_url(self):
+        return reverse_lazy('student_list')
